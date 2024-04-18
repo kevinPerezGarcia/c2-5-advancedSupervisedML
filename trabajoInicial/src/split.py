5a6f462d1bf0c7988501b1a34a5712107429cc77
Kevin Perez Garcia <econ.perez.garcia.k@gmail.com>
2024-04-18 07:49:41 -0500

    trabajoInicial/src/split.py: Añadiendo conjunto de validación

--- trabajoInicial/src/split.py
+++ trabajoInicial/src/split.py
@@ -7,9 +7,12 @@ target = 'ATTRITION'
 X = df.drop(target, axis=1)
 y = df[[target]]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=2024)
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, stratify=y, random_state=2024)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, stratify=y_temp, random_state=2024)
 
 X_train.to_pickle("trabajoInicial/data/interim/X_train.pkl")
+X_val.to_pickle("trabajoInicial/data/interim/X_val.pkl")
 X_test.to_pickle("trabajoInicial/data/interim/X_test.pkl")
 y_train.to_pickle("trabajoInicial/data/interim/y_train.pkl")
+y_val.to_pickle("trabajoInicial/data/interim/y_val.pkl")
 y_test.to_pickle("trabajoInicial/data/interim/y_test.pkl")
