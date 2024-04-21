55c08f80f3dbd5d868d79a0c362c30e6e89f53fe
Kevin Perez Garcia <econ.perez.garcia.k@gmail.com>
2024-04-20 20:42:59 -0500

    trabajoInicial/src/split.py: Quitando orden de las variables

--- trabajoInicial/src/split.py
+++ trabajoInicial/src/split.py
@@ -5,24 +5,6 @@ df = pd.read_csv("trabajoInicial/data/raw/data.csv", sep=';')
 
 df.drop(['ATTRITION', 'CODMES'], axis=1, inplace=True)
 
-# Lista de columnas específicas a colocar al principio
-columnas_al_inicio = [ 'ATTRITION']
-
-# Obtener una lista de todas las columnas en el DataFrame
-todas_las_columnas = df.columns.tolist()
-
-# Obtener una lista de las columnas que no están en la lista de columnas específicas
-columnas_restantes = [col for col in todas_las_columnas if col not in columnas_al_inicio]
-
-# Ordenar alfabéticamente las columnas restantes
-columnas_restantes.sort()
-
-# Concatenar la lista de columnas específicas al principio con las columnas restantes ordenadas alfabéticamente
-nuevo_orden_columnas = columnas_al_inicio + columnas_restantes
-
-# Reorganizar las columnas del DataFrame en el nuevo orden
-df = df.reindex(columns=nuevo_orden_columnas)
-
 target = 'ATTRITION'
 X = df.drop(target, axis=1)
 y = df[[target]]
