682ad7db354bcb519540eb38231a07c7a1cafbe3
Kevin Perez Garcia <econ.perez.garcia.k@gmail.com>
2024-04-20 20:41:58 -0500

    trabajoInicial: quitar archivos innecesarios
    - trabajoInicial/src/extract_columns.py
    - trabajoInicial\references\trabajoReferencial\code-alumno-lgbm.ipynb

--- trabajoInicial/src/extract_columns.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_pickle("trabajoInicial/data/interim/data.pkl")
-
-# Crear DataFrame con los nombres originales de las variables
-df_columns = pd.DataFrame(df.columns, columns=['Variable original'])
-
-df_columns.to_excel('trabajoInicial/references/seguimientoVariables.xlsx', index=False, sheet_name='seguimientoVariables')
