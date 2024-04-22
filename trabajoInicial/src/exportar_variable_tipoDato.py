9275403bf5b33eca7e5248bf95357eaae1e96ceb
Kevin Perez Garcia <econ.perez.garcia.k@gmail.com>
2024-04-22 10:06:40 -0500

    trabajoInicial/src: exportar_variable_tipoDato.py refactorizado

--- trabajoInicial/src/exportar_variable_tipoDato.py
+++ trabajoInicial/src/exportar_variable_tipoDato.py
@@ -4,6 +4,4 @@ df = pd.read_pickle("trabajoInicial/data/interim/data.pkl")
 df_tipoDato = df.dtypes.reset_index()
 df_tipoDato.columns = ['Variable', 'Tipo de dato original']
 
-excel_file = "trabajoInicial/references/seguimientoVariables.xlsx"
-sheet_name = "seguimientoVariables"
-df_tipoDato.to_excel(excel_file, index=False, sheet_name=sheet_name)
+df_tipoDato.to_excel("trabajoInicial/references/seguimientoVariables.xlsx", index=False, sheet_name="seguimientoVariables")
