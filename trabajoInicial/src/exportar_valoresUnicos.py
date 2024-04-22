00ee5f52b4d26a8069320ae973665a8f403b839a
Kevin Perez Garcia <econ.perez.garcia.k@gmail.com>
2024-04-22 10:08:45 -0500

    trabajoInicial/src/exportar_valoresUnicos.py: Usando paquete personal

--- trabajoInicial/src/exportar_valoresUnicos.py
+++ trabajoInicial/src/exportar_valoresUnicos.py
@@ -1,11 +1,7 @@
 import pandas as pd
 
-excel_file = "trabajoInicial/references/seguimientoVariables.xlsx"
-sheet_name = "seguimientoVariables"
-df_excel =pd.read_excel(excel_file)
-
 df = pd.read_pickle("trabajoInicial/data/interim/data.pkl")
 df_valoresUnicos = pd.DataFrame(df.nunique().reset_index(drop=True), columns=['Número de valores únicos'])
 
-df_excel_concated = pd.concat([df_excel, df_valoresUnicos], axis=1)
-df_excel_concated.to_excel(excel_file, index=False, sheet_name=sheet_name)
+from kenpkgs.report_excel import report_df_to_excel
+report_df_to_excel(df=df_valoresUnicos, filename='trabajoInicial/references/seguimientoVariables.xlsx')
